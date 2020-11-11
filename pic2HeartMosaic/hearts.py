a554136fa7745ad0555e18a26bae770a98c05225
Valentino Lugli <valentinolugli@gmail.com>
2020-11-11 22:12:31 +0100

    [new] Added new project!
    -Still pretty much not done but it's just a small thingy,
    almost like a toy.
    
    -Not much details for now

---
+++ pic2HeartMosaic/hearts.py
@@ -0,0 +1,29 @@
+#   ESPAÑOL
+#       Titulo: Pic to Heart
+#       Fecha: 11/11/2020
+#       Autor: Valentino Lugli
+#       Version: 0.01
+#       Descripción: WIP
+#       Uso: WIP
+
+#   ENGLISH
+#       Title: Pic to Heart
+#       Date: 11/11/2020
+#       Author: Valentino Lugli
+#       Version: 0.01
+#       Description: WIP
+#       Usage: WIP
+
+# Libraries
+#   Image manipulation
+from PIL import Image
+
+
+# Main Code
+
+imgName = '20.jpg'
+# img = input()
+
+img = Image.open(imgName)
+
+# Windows got a problem wih NumPy so I'll work it later on Linux
