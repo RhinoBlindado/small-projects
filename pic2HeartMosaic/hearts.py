09409e62e4bf41657f30d816cc085e7b671b7a5d
Valentino Lugli <valentinolugli@gmail.com>
2020-11-12 16:55:22 +0100

    [feat] Code is working, basic functionality achieved.
    - Added the functionality that converts RBG values to colored heart emojis as per their aproximate color.
    	- Quite bugged as for now.

--- pic2HeartMosaic/hearts.py
+++ pic2HeartMosaic/hearts.py
@@ -15,15 +15,50 @@
 #       Usage: WIP
 
 # Libraries
-#   Image manipulation
+#   To load the image
 from PIL import Image
+
+#   To convert the image to an array.
+from numpy import asarray
+
+
+
+# Functions
+def nearestColor(hexVal,hearts):
+    
+
+    minDiff = 0xffffff
+    for i in hearts:
+        if abs(i - int(hexVal, 16)) < minDiff:
+            selection = i
+            minDiff = abs(i - int(hexVal, 16))
+    
+    return selection
+
 
 
 # Main Code
 
-imgName = '20.jpg'
+imgName = '1.png'
 # img = input()
 
-img = Image.open(imgName)
+#       Red,                    Orange
+hearts={0xff0000 : "\U00002764",0xf88606 : "\U0001F9E1" ,
+#       Yellow,                 Green,
+        0xfbc02d : "\U0001F49B",0x00ff00 : "\U0001F49A" ,
+#       Blue,                   Purple,
+        0x0000ff : "\U0001F499",0x8321a5 : "\U0001F49C",
+#       Brown,                  Black,
+        0x835f41 : "\U0001F90E",0x000000 : "\U0001F5A4",
+#       White
+        0xffffff : "\U0001F90D"}
 
-# Windows got a problem wih NumPy so I'll work it later on Linux
+imgArray = asarray(Image.open(imgName))
+
+for i in range(imgArray.shape[0]):
+    for j in range(imgArray.shape[1]):
+        hexVal = '0x%02x%02x%02x' % (imgArray[i][j][0], imgArray[i][j][1], imgArray[i][j][2])
+        out = nearestColor(hexVal,hearts)
+        print(hearts[out], end="")
+    print("\n")
+
